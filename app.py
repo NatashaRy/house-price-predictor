24835772c8097528ce7c0cf254830bf80167adbf
NatashaRy <natasha@natasharydell.se>
2025-08-13 23:49:58 +0200

    Feat: Finished hypotheses page

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from app_pages.multipage import MultiPage
 # Import pages
 from app_pages.summary import summary_body
 from app_pages.analysis import analysis_body
+from app_pages.hypotheses import hypotheses_body
 
 # Initialize MultiPage app
 app = MultiPage(app_name="House Price Predictor")
@@ -14,6 +15,7 @@ app = MultiPage(app_name="House Price Predictor")
 # Add page to the app
 app.add_page("Project Overview", summary_body)
 app.add_page("Correlation Analysis", analysis_body)
+app.add_page("Hypotheses and Validation", hypotheses_body)
 
 # Run the app
 app.run()
